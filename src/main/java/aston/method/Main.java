3ea417c386aac94d36460229ee4349aa5a1dceb4
Roman Norkin <vbybrhfan@yandex.ru>
2024-08-08 19:03:14 +0300

    task-10

--- src/main/java/aston/method/Main.java
+++ src/main/java/aston/method/Main.java
@@ -1,8 +1,27 @@
 package aston.method;
 
+import java.util.Arrays;
+
 public class Main {
-
     public static void main(String[] args) {
+        invert();
+    }
 
+    /*
+     *  Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+     *  С помощью цикла и условия заменить 0 на 1, 1 на 0;
+     */
+
+    private static void invert() {
+        int[] arr = {0, 1, 0, 1, 1, 0, 1, 0, 0,};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
