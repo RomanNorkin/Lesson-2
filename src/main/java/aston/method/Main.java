d1a631f7fc446684895241733d1c9a5ef256c117
Roman Norkin <vbybrhfan@yandex.ru>
2024-08-08 19:17:51 +0300

    task-12

--- src/main/java/aston/method/Main.java
+++ src/main/java/aston/method/Main.java
@@ -1,8 +1,26 @@
 package aston.method;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        task6();
 
+        /*
+         * Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ]
+         * пройти по нему циклом, и числа меньшие 6 умножить на 2;
+         */
+    }
+
+    public static void task6() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] *= 2;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
