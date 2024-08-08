1c55a703aa7e0363e543fa175720075fb0f49abc
Roman Norkin <vbybrhfan@yandex.ru>
2024-08-08 18:02:25 +0300

    task-4

--- src/main/java/aston/method/Main.java
+++ src/main/java/aston/method/Main.java
@@ -3,6 +3,23 @@ package aston.method;
 public class Main {
 
     public static void main(String[] args) {
+        compareNumbers();
+    }
 
+    /*
+     * Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b,
+     * и инициализируйте их любыми значениями, которыми захотите. Если a больше или равно b,
+     * то необходимо вывести в консоль сообщение “a >= b”, в противном случае “a < b”;
+     */
+
+    private static void compareNumbers() {
+        int a = 5;
+        int b = 11;
+
+        if (a >= b) {
+            System.out.println(a + " >= " + b);
+        } else {
+            System.out.println(a + " < " + b);
+        }
     }
 }
