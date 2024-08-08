17a5325ace76adb592e02857d65684c5ddaadaf9
Roman Norkin <vbybrhfan@yandex.ru>
2024-08-08 19:05:53 +0300

    task-11

--- src/main/java/aston/method/Main.java
+++ src/main/java/aston/method/Main.java
@@ -1,8 +1,23 @@
 package aston.method;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        fillArrey();
     }
+
+    /*
+     * Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 ... 100;
+     */
+
+    private static void fillArrey() {
+        int[] arr = new int[100];
+        for (int i = 0; i < 100; i++) {
+            arr[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
 }
