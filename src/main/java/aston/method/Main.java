ed47c3bba4ddc7b77bb60a6ef8a346f17284fa8c
Roman Norkin <vbybrhfan@yandex.ru>
2024-08-08 20:36:32 +0300

    task-14

--- src/main/java/aston/method/Main.java
+++ src/main/java/aston/method/Main.java
@@ -1,8 +1,24 @@
 package aston.method;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(task14(5, 2)));
+    }
 
+    /*
+     *  Написать метод, принимающий на вход два аргумента:
+     *  len и initialValue, и возвращающий одномерный массив типа int длиной len,
+     *  каждая ячейка которого равна initialValue.
+     */
+
+    private static int[] task14(int size, int value) {
+        int[] matrix = new int[size];
+        for (int i = 0; i < size; i++) {
+            matrix[i] = value;
+        }
+        return matrix;
     }
 }
