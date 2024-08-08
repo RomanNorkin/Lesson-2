248bb9c39c9f0b512e2a8f0e9c396585981f2d0f
Roman Norkin <vbybrhfan@yandex.ru>
2024-08-08 18:32:46 +0300

    task-8

--- src/main/java/aston/method/Main.java
+++ src/main/java/aston/method/Main.java
@@ -3,6 +3,17 @@ package aston.method;
 public class Main {
 
     public static void main(String[] args) {
+        task8(15, "JAVA");
+    }
 
+    /*
+     * Напишите метод, которому в качестве аргументов передается строка и число,
+     * метод должен отпечатать в консоль указанную строку, указанное количество раз;
+     */
+
+    private static void task8(int number, String word) {
+        for (int i = 1; i <= number; i++) {
+            System.out.println(word);
+        }
     }
 }
