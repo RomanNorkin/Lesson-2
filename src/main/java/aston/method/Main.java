b3a0520a32c54812e9d53fc90bac869c9b4149e6
Roman Norkin <vbybrhfan@yandex.ru>
2024-08-08 18:56:25 +0300

    task-9

--- src/main/java/aston/method/Main.java
+++ src/main/java/aston/method/Main.java
@@ -4,5 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
+        yearCheck(400);
+    }
+
+    /*
+     * Напишите метод, который определяет, является ли год високосным,
+     * и возвращает boolean (високосный - true, не високосный - false).
+     * Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+     */
+
+    private static void yearCheck(int year) {
+
+        if (!(year % 4 == 0) || ((year % 100 == 0) && !(year % 400 == 0))) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
