6d169236b2ce4ce2331f955840df86dbe060f39e
Roman Norkin <vbybrhfan@yandex.ru>
2024-08-08 18:30:37 +0300

    task-7

--- src/main/java/aston/method/Main.java
+++ src/main/java/aston/method/Main.java
@@ -3,6 +3,19 @@ package aston.method;
 public class Main {
 
     public static void main(String[] args) {
+        checkNumber2(45);
+    }
 
+    /*
+     * Напишите метод, которому в качестве параметра передается целое число.
+     * Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
+     * Замечание: ноль считаем положительным числом.
+     */
+
+    private static void checkNumber2(int number) {
+
+        if (number >= 0) {
+            System.out.println("false");
+        } else System.out.println("true");
     }
 }
