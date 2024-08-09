da4a050a277a1a7940ad4f701c7b528d2b235b36
Roman Norkin <92871235+RomanNorkin@users.noreply.github.com>
2024-08-09 13:03:28 +0300

    Add files via upload

---
+++ lessonNo2/src/main/java/aston/method/Main.java
@@ -0,0 +1,17 @@
+package aston.method;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    /*
+     * Создайте метод printThreeWords(),
+     * который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
+     */
+
+    private static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+}
