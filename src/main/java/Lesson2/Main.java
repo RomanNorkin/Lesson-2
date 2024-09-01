57b8f7ac0c64457d986a4a634ca7479b57e5cb21
Roman Norkin <vbybrhfan@yandex.ru>
2024-09-01 14:12:00 +0300

    Lesson-2

--- src/main/java/Lesson2/Main.java
+++ src/main/java/Lesson2/Main.java
@@ -1,4 +1,38 @@
 package Lesson2;
 
+import java.util.Arrays;
+
+import static Lesson2.Lesson2.*;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1.\n");
+        printThreeWords();
+        System.out.println("\nЗадание 2.\n");
+        checkSumSign();
+        System.out.println("\nЗадание 3.\n");
+        printColor();
+        System.out.println("\nЗадание 4.\n");
+        compareNumbers();
+        System.out.println("\nЗадание 5.\n");
+        check();
+        System.out.println("\nЗадание 6.\n");
+        checkNumber(10);
+        System.out.println("\nЗадание 7.\n");
+        checkNumber2(45);
+        System.out.println("\nЗадание 8.\n");
+        task8(15, "JAVA");
+        System.out.println("\nЗадание 9.\n");
+        yearCheck(400);
+        System.out.println("\nЗадание 10.\n");
+        invert();
+        System.out.println("\nЗадание 11.\n");
+        fillArrey();
+        System.out.println("\nЗадание 12.\n");
+        task12();
+        System.out.println("\nЗадание 13.\n");
+        task13(10, 5);
+        System.out.println("\nЗадание 14.\n");
+        System.out.println(Arrays.toString(task14(10,3)));
+    }
 }
