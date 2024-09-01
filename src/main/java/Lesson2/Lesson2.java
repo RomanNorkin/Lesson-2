ef536c37fc18011fdd97c083af8af7b6ae1d236e
Roman Norkin <vbybrhfan@yandex.ru>
2024-09-01 13:41:31 +0300

    Lesson-2

--- src/main/java/Lesson2/Lesson2.java
+++ src/main/java/Lesson2/Lesson2.java
@@ -1,4 +1,32 @@
 package Lesson2;
 
 public class Lesson2 {
+
+    /*
+     * Создайте метод printThreeWords(),
+     * который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
+     */
+
+    private static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+
+    /*
+     * Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b,
+     * и инициализируйте их любыми значениями, которыми захотите.
+     * Далее метод должен просуммировать эти переменные, и если их сумма больше или равна 0,
+     * то вывести в консоль сообщение “Сумма положительная”, в противном случае - “Сумма отрицательная”;
+     */
+
+    private static void checkSumSign() {
+        int a = -11;
+        int b = 5;
+
+        if ((a + b) >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
 }
